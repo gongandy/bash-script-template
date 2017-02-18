89f792efa7890e005df4df11d1417fc6790552bf
gongandy <gongandy@macOS.local>
2017-02-18 15:15:31 +0800

    Andy:revise for mac

--- source.sh
+++ source.sh
@@ -70,14 +70,14 @@ function script_init() {
 
     # Text attributes
     readonly ta_none="$(tput sgr0 || true)"
-    declare -g ta_bold ta_uscore ta_blink ta_reverse ta_conceal
+    declare -a ta_bold ta_uscore ta_blink ta_reverse ta_conceal
 
     # Foreground codes
-    declare -g fg_black fg_blue fg_cyan fg_green \
+    declare -a fg_black fg_blue fg_cyan fg_green \
                fg_magenta fg_red fg_white fg_yellow
 
     # Background codes
-    declare -g bg_black bg_blue bg_cyan bg_green \
+    declare -a bg_black bg_blue bg_cyan bg_green \
                bg_magenta bg_red bg_white bg_yellow
 }
 
